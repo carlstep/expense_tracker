6e364de6428f9029a8faa8187aee50700eac76db
carlstep <carlstep333@me.com>
2023-05-25 07:40:35 +0700

    delete expense item, managing snackbars

--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -1,24 +1,33 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 
-import 'package:expense_tracker/widgets/expenses_list/expenses_item.dart';
 import 'package:flutter/material.dart';
 
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/expenses_list/expenses_item.dart';
 
 class ExpensesList extends StatelessWidget {
   const ExpensesList({
     super.key,
     required this.expenses,
+    required this.onRemoveExpense,
   });
 
   final List<Expense> expenses;
+
+  final void Function(Expense expense) onRemoveExpense;
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: expenses.length,
-      itemBuilder: (ctx, index) => ExpenseItem(
-        expenses[index],
+      itemBuilder: (ctx, index) => Dismissible(
+        onDismissed: (direction) => onRemoveExpense(expenses[index]),
+        key: ValueKey(
+          expenses[index],
+        ),
+        child: ExpenseItem(
+          expenses[index],
+        ),
       ),
     );
   }
